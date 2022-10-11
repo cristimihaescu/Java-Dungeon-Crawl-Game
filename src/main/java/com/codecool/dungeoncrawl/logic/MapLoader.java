9435fce4ab2756924e88767017f4eb99612dc4c9
Maxim Copita <copita.maxim.info@gmail.com>
2022-10-11 15:01:37 +0300

    add door on the map

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -1,5 +1,6 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.actors.Door;
 import com.codecool.dungeoncrawl.logic.actors.Key;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
@@ -34,7 +35,7 @@ public class MapLoader {
                             break;
                         case 's':
                             cell.setType(CellType.FLOOR);
-                            new Skeleton(cell);
+                            map.setSkeleton(new Skeleton(cell));
                             break;
                         case 'k':
                             cell.setType(CellType.KEY);
@@ -43,6 +44,10 @@ public class MapLoader {
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
+                            break;
+                        case 'd':
+                            cell.setType(CellType.DOOR);
+                            new Door(cell);
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
